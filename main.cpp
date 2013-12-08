fa019d5772db17aec801cf046a476c67de3e14bd
Jacob Buysse <Jacob.Buysse@gmail.com>
2013-12-08 15:03:42 -0600

    Point spread and styles.
    Signed-off-by: Jacob Buysse <Jacob.Buysse@gmail.com>

--- main.cpp
+++ main.cpp
@@ -14,8 +14,9 @@ std::string GenerateHtmlPage(const std::string& body)
 	out << "<meta name=\"description\" content=\"Sheepshead Scores\">";
 	out << "<link href=\"//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css\" rel=\"stylesheet\">";
 	out << "<link href=\"//netdna.bootstrapcdn.com/font-awesome/4.0.1/css/font-awesome.min.css\" rel=\"stylesheet\">";
-	out << "</head><body>";
+	out << "</head><body><div class=\"container\">";
 	out << body;
+	out << "</div>";
 	out << "<script src=\"//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js\"></script>";
 	out << "<script src=\"//netdna.bootstrapcdn.com/bootstrap/3.0.3/js/bootstrap.min.js\"></script>";
 	out << "<script src=\"//ajax.googleapis.com/ajax/libs/angularjs/1.2.4/angular.min.js\"></script>";
@@ -23,32 +24,41 @@ std::string GenerateHtmlPage(const std::string& body)
 	return out.str();
 }
 
+std::string GeneratePlayer(int number)
+{
+	std::ostringstream out;
+	out << "<div class=\"form-group\">"
+		<< "<div class=\"col-sm-3\">"
+		<< "<input type=\"text\" class=\"form-control\" name=\"player" << number << "Name\" placeholder=\"Name\">"
+		<< "</div>"
+		<< "<div class=\"col-sm-2\">"
+		<< "<input type=\"number\" class=\"form-control\" name=\"player" << number << "Score\" placeholder=\"Score\">"
+		<< "</div>"
+		<< "</div>";
+	return out.str();
+}
+
 HttpResponse DoGet(const HttpRequest& request)
 {
 	std::ostringstream out;
-	out << "<div class=\"container\">";
 	out << "<div class=\"jumbotron\">";
+	
 	out << "<h1>Sheepshead Scores</h1>";
 	out << "<p>Enter today's Sheepshead scores and get daily, MTD, and YTD totals.</p>";
 	
-	out << "<form class=\"form-horizontal\" role=\"form\" action=\"" << request.GetScriptName() << "\" method=\"post\">"
-		<< "<div class=\"form-group\">"
-		<< "<div class=\"col-sm-3\">"
-		<< "<input type=\"text\" class=\"form-control\" name=\"player1Name\" placeholder=\"Name\">"
-		<< "</div>"
+	out << "<form class=\"form-horizontal\" role=\"form\" action=\"" << request.GetScriptName() << "\" method=\"post\">";
+	for (auto number = 1; number <= 6; ++number)
+		out << GeneratePlayer(number);
+	out << "<div class=\"form-group\">"
 		<< "<div class=\"col-sm-2\">"
-		<< "<input type=\"number\" class=\"form-control\" name=\"player1Score\" placeholder=\"Score\">"
-		<< "</div>"
-		<< "</div>"
-		
-		<< "<div class=\"form-group\">"
-		<< "<div class=\"col-sm-3\">"
 		<< "<button type=\"submit\" class=\"btn btn-default btn-primary\">Submit</button>"
+		<< "</div>"
+		<< "<div class=\"col-sm-3\">"
+		<< "<p class=\"form-control-static\" style=\"text-align: right;\">P.S. <span>10</span></p>"
 		<< "</div>"
 		<< "</div>"
 		<< "</form>";
 		
-	out << "</div>";
 	out << "</div>";
 	
 	return { "text/html", GenerateHtmlPage(out.str()) };
